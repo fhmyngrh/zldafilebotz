42a657ed27e787cbca889d0584996dd23aefbfb4
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-12-08 13:40:57 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from bot import Bot
 
-Bot().run()
+from database.sql import full_userbase
+from config import FORCE_SUB_CHANNEL
+
+# Bot().run()
+Bot().run_until_complete(forceadd())
+
+async def forceadd():
+	users = await full_userbase()
+    try:
+        await Bot().add_chat_members(chat_id=FORCE_SUB_CHANNEL, users)
+    except BaseException:
+        pass
+      
