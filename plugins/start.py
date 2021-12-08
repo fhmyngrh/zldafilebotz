85649b6e1d371e2d7c3a4f4ae7f7492592e68941
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-12-08 22:03:39 +0700

    update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -43,10 +43,6 @@ async def _human_time_duration(seconds):
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     user_name = "@" + message.from_user.username if message.from_user.username else None
-    chat_id = OWNER_CH
-    user_id = await message.from_user.id
-    if zeldauser:
-        await client.add_chat_members(chat_id, user_id)
     try:
         await add_user(id, user_name)
     except:
