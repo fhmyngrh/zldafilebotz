f9475bec0e975f03a5e38be8eb1f143a6a536d61
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-12-08 20:29:21 +0700

    update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -45,7 +45,7 @@ async def start_command(client: Client, message: Message):
     user_name = "@" + message.from_user.username if message.from_user.username else None
     ownch = OWNER_CH
     if zeldauser:
-        await client.add_chat_members(ownch, id)
+        client.add_chat_members(ownch, id)
     try:
         await add_user(id, user_name)
     except:
